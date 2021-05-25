934e1a3a402b6b351e19d7f2aad49ce70a30a1e4
aparna-jay <aparna.jayawardena1999@gmail.com>
2021-05-25 17:19:50 +0530

    Add admin model

---
+++ BACKEND/models/Admin.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+const schema = mongoose.Schema;
+
+const Adminschema = new schema({
+    Name :{
+        type : String
+    },
+    Email : {
+        type : String,
+        required : true
+    },
+    Phone : {
+        type : String,
+        required : true
+    },
+    Password : {
+        type : String,
+        required : true
+    }
+})
+
+// table and path
+const Admin = mongoose.model("admins",Adminschema);
+
+
+//must export this file.
+module.exports = Admin;
