9cab9cc7336750dc9aaa77820cc891972fb2891e
aparna-jay <aparna.jayawardena1999@gmail.com>
2021-05-26 11:13:23 +0530

    create front end structure

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import "App.css";
+import img from "./img.jpg"
+
+const App =()=> (
+  <div>
+      <h1 className="heading">sadsdgdfg</h1>
+      <img src={img} alt="" />
+  </div>
+);
+
+export default App;
