6321360146223b7e057c0bb8fdd7425a6534cc37
gavin <gavingunapala06@gmail.com>
2021-05-27 10:10:53 +0530

    Reviewerhome created

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import "App.css";
 import "bootstrap.min.css";
-import img from "./img.jpg"
+// import img from "./img.jpg"
 import MainNav from "./component/navbar/MainNav";
+import Reviewerhome from "./component/Reviewerhome";
 
 const App =()=> (
   <div>
+
       <MainNav></MainNav>
       <h1 className="heading">sadsdgdfg</h1>
-      <img src={img} alt="" />
+      {/*<img src={img} alt="" />*/}
+      <Reviewerhome></Reviewerhome>
   </div>
 );
 
