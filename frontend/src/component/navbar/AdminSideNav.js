e1b7a5375aa2af8ada053447ffe80037768909ad
aparna-jay <aparna.jayawardena1999@gmail.com>
2021-06-03 22:52:43 +0530

    Add manage admins code

--- frontend/src/component/navbar/AdminSideNav.js
+++ frontend/src/component/navbar/AdminSideNav.js
@@ -2,8 +2,6 @@ import React  from "react"
 import "../../styles/Admin/sidebar.css"
 
 const AdminSideNav = () => {
-
-
         return (
             <div>
                     <nav id="sidebar" >
@@ -13,6 +11,10 @@ const AdminSideNav = () => {
                         <ul className="list-unstyled components">
                             <li>
                                 <a href="/admin">Home</a>
+                                <a href="/ManageAdmins">Manage Admins</a>
+                                <a href="/ManageReviewers">Manage Reviewers</a>
+                                <a href="/ManageEditors">Manage Editors</a>
+
                                 <a href="#">Conference Details</a>
                             </li>
                             <li>
