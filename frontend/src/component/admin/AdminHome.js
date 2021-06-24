db2ab8d9f649187b2f6e290ca8a08a0eae41b478
aparna-jay <aparna.jayawardena1999@gmail.com>
2021-06-24 20:53:02 +0530

    Change admin home page

--- frontend/src/component/admin/AdminHome.js
+++ frontend/src/component/admin/AdminHome.js
@@ -1,10 +1,31 @@
-import React from "react"
+import React, {useEffect, useState} from "react"
 import '../../styles/Admin/table.css'
 import AdminSideNav from "../navbar/AdminSideNav";
 import img from "../../img.jpg";
+import {useHistory} from "react-router-dom";
+import axios from "axios";
 
-const AdminHome = (loggedUser) => {
+const AdminHome = ({loggedUser}) => {
      console.log(loggedUser)
+
+    const id = loggedUser;
+    const [admin , setAdmin] = useState( []);
+
+
+    //get logged admin
+    useEffect(()=>{
+        function getAdmin(){
+            axios.get("http://localhost:8070/admin/get/"+id).then((res)=>{
+                setAdmin(res.data);
+                // setName(res.data.name);
+                console.log(res.data);
+            }).catch((err)=>{
+            })
+        }
+        getAdmin();
+    },[loggedUser]);
+
+
 
     return (
         <div>
@@ -14,7 +35,7 @@ const AdminHome = (loggedUser) => {
                         <AdminSideNav />
                     </div>
                     <div className="col-md-6 col-xl-3" >
-                        <center><h1>Welcome {loggedUser.Name}</h1></center>
+                        <center><h1>Welcome {admin.Name}</h1></center>
                         <br />
                     <main className="page">
                         <section className="clean-block about-us">
