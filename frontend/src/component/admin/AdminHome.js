e7214739484eabfbecabf7bea581f5314fb743d4
aparna-jay <aparna.jayawardena1999@gmail.com>
2021-06-28 16:40:54 +0530

    Update Admin Profile

--- frontend/src/component/admin/AdminHome.js
+++ frontend/src/component/admin/AdminHome.js
@@ -6,7 +6,9 @@ import axios from "axios";
 
 const AdminHome = () => {
     const [admin , setAdmin] = useState( []);
-
+    const[phone, setPhone]= useState("");
+    const[email, setEmail]= useState("");
+    const[name, setName]= useState("");
 
     //get logged admin
     useEffect(()=>{
@@ -22,7 +24,27 @@ const AdminHome = () => {
         getAdmin();
     },[]);
 
+    const phoneSetter = (e) => {
+        setPhone(e.target.value);
+    }
+    const emailSetter = (e) => {
+        setEmail(e.target.value);
+    }
+    const nameSetter = (e) => {
+        setName(e.target.value);
+    }
 
+    const UpdateProfile = ()=>{
+        const newAdmin= {
+            Name: name,
+            Phone:phone,
+            Email:email
+        };
+        axios.put('http://localhost:8070/admin/updateOne/'+ admin._id , newAdmin).then(() =>{
+            alert("Updated successfully!!!")
+        }).catch((err) =>{
+        });
+    }
 
     return (
         <div>
@@ -31,9 +53,10 @@ const AdminHome = () => {
                     <div className="col-md-6 col-xl-4" >
                         <AdminSideNav />
                     </div>
-                    <div className="col-md-6 col-xl-3" >
+                    <div className="col-md-6 col-xl-6" >
+                        <br /> <br />
                         <center><h1>Welcome {admin.Name}</h1></center>
-                        <br />
+                        <br /> <br />
                     <main className="page">
                         <section className="clean-block about-us">
                             <div className="row justify-content-center">
@@ -47,7 +70,7 @@ const AdminHome = () => {
                                                     <div className="col">
                                                         <p className="labels"><strong>Name</strong></p>
 
-                                                        <input className="form-control" type="text" placeholder={admin.Name} />
+                                                        <input className="form-control" type="text" placeholder={admin.Name} onChange={nameSetter} />
                                                     </div>
 
                                                 </div>
@@ -56,7 +79,7 @@ const AdminHome = () => {
                                                     <div className="col">
                                                         <p className="labels"><strong>Email</strong></p>
 
-                                                        <input className="form-control" type="email" placeholder={admin.Email} />
+                                                        <input className="form-control" type="email" placeholder={admin.Email} onChange={emailSetter} />
                                                     </div>
                                                 </div>
                                                 <br />
@@ -64,14 +87,24 @@ const AdminHome = () => {
                                                     <div className="col">
                                                         <p className="labels"><strong>Phone</strong></p>
 
-                                                        <input className="form-control" type="text" placeholder={admin.Phone} />
+                                                        <input className="form-control" type="text" placeholder={admin.Phone} onChange={phoneSetter} />
                                                     </div>
                                                 </div>
                                                 <br />
                                                 <div className="row">
                                                     <div className="col-md-12">
-                                                        <a className="btn btn-success" role="button" href="#">
-                                                            <i className="fas fa-pencil-alt"></i>&nbsp;Update</a></div>
+                                                        <button className="btn btn-primary d-block w-100 regButton" type="submit"
+                                                                onClick={()=>UpdateProfile()}>Update
+                                                        </button>
+                                                    </div>
+                                                    <br /><br />
+
+                                                    <div className="col-md-12">
+                                                        <button className="btn btn-danger d-block w-100" type="submit"
+                                                                onClick={()=>DeleteProfile(admin._id)}>Delete
+                                                        </button>
+                                                    </div>
+                                                    <br /><br />
                                                 </div>
                                             </div>
                                         </div>
