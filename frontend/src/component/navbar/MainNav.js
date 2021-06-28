f8bdbf7080bcac07339d23697ca153a95d2b71f0
Kasuni_Madhushika <kasunimadhushika77@gmail.com>
2021-06-29 03:32:54 +0530

    Home

--- frontend/src/component/navbar/MainNav.js
+++ frontend/src/component/navbar/MainNav.js
@@ -7,12 +7,39 @@ const MainNav = () =>{
 
    <div className="container">
 
+       {/*<nav className="navbar navbar-dark navbar-expand-lg fixed-top bg-dark" id="mainNav">*/}
+       {/*    <div className="container"><a className="navbar-brand" href="#page-top">Brand</a>*/}
+       {/*        <button data-bs-toggle="collapse" data-bs-target="#navbarResponsive"*/}
+       {/*                className="navbar-toggler navbar-toggler-right" type="button" aria-controls="navbarResponsive"*/}
+       {/*                aria-expanded="false" aria-label="Toggle navigation"><i className="fa fa-bars"></i></button>*/}
+       {/*        <div className="collapse navbar-collapse" id="navbarResponsive">*/}
+       {/*            <ul className="navbar-nav ms-auto text-uppercase">*/}
+       {/*                <li className="nav-item"><a className="nav-link" href="/Home">Home</a></li>*/}
+       {/*                <li className="nav-item"><a className="nav-link" href="Keynotes">Keynote</a></li>*/}
+       {/*                <li className="nav-item"><a className="nav-link" href="/Conference">Conference</a></li>*/}
+       {/*                <li className="nav-item"><a className="nav-link" href="/login">Login</a></li>*/}
+       {/*                <li className="nav-item"><a className="nav-link" href="/admin">Admin</a></li>*/}
+       {/*                <li className="nav-item"><a className="nav-link" href="/reviewer">reviewer</a></li>*/}
+       {/*                <li className="nav-item"><a className="nav-link" href="/Editor">Editor</a></li>*/}
+       {/*                <li className="nav-item"><a className="nav-link" href="/user">User</a></li>*/}
+       {/*                <li className="nav-item"><a className="nav-link" href="/downloadPapers">Download Papers</a></li>*/}
+       {/*                <li className="nav-item"><a className="nav-link" href="/UserProfile">UserProfile</a></li>*/}
+       {/*                <li className="nav-item"><a className="nav-link" href="/WorkshopProfile">WorkshopProfile</a></li>*/}
+       {/*                <li className="nav-item"><a className="nav-link" href="/AttendeeProfile">AttendeeProfile</a></li>*/}
+       {/*                <li className="nav-item"><a className="nav-link" href="/Keynotes-profile">Keynotes Profile</a></li>*/}
+       {/*                <li className="nav-item"><a className="nav-link" href="/ConferenceDashboard">Conference Dashboard</a></li>*/}
+       {/*                <li className="nav-item"><a className="nav-link" href="/KeynoteDashboard">Keynote Dashboard</a></li>*/}
+       {/*            </ul>*/}
+       {/*        </div>*/}
+       {/*    </div>*/}
+       {/*</nav>*/}
+
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
             <div className="container-fluid">
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul className="navbar-nav me-auto mb-2 mb-lg-0" id="list">
                         <li className="nav-item">
-                            <a className="nav-link active" aria-current="page" href="#">Home</a>
+                            <a className="nav-link active" aria-current="page" href="/Home">Home</a>
                         </li>
                         <li className="nav-item">
                             <a className="nav-link active" aria-current="page" href="Keynotes">Keynote</a>
