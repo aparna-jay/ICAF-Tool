8e88f0fbd468f355a95987d82583876754f34713
Kasuni_Madhushika <kasunimadhushika77@gmail.com>
2021-06-29 18:00:06 +0530

    Retrieve home

--- frontend/src/component/Home/Home.js
+++ frontend/src/component/Home/Home.js
@@ -5,6 +5,46 @@ import {useParams} from "react-router";
 import axios from "axios";
 
 const Home = () => {
+    // const Status = "";
+    const [Conference, setConference] = useState([]);
+    const [Keynotes, setKeynotes] = useState([]);
+
+    useEffect(() => {
+        loadConference();
+        loadKeynotes();
+    }, [Conference,Keynotes]);
+
+    //  let cancel;
+    const loadConference = async () => {
+        //    setLoading(true);
+        try{
+            let query = `?Status[in]=Approved`;
+            const {data} = await axios({
+                method: "GET",
+                url: `http://localhost:8070/Conference/sort${query}`,
+                //   cancelToken: new axios.CancelToken((c) => cancel = c)
+            });
+            setConference(data.data.conference);
+            //setLoading(false);
+            console.log(data.conference);
+        }catch (error){
+            console.log(error.response.data);
+        }
+    };
+
+    const loadKeynotes = async () => {
+
+        await axios
+            .get('http://localhost:8070/KeyNoteSpeakers/')
+            .then((res) => {
+                console.log(res.data);
+                setKeynotes(res.data);
+            })
+            .catch((err) => {
+                alert(err.response.data);
+            });
+    };
+
 
     return(
         <div>
@@ -24,65 +64,39 @@ const Home = () => {
                         <div className="container">
                             <div className="row">
                                 <div className="col-lg-12 text-center" id="home">
-                                    <h1 className="text-uppercase section-heading">Conferences</h1>
-                                    <h2 className="text-muted section-subheading">ICAF 2021</h2>
+                                    <h1 className="text-uppercase section-heading">International Conferences Application Frameworks - 2021</h1>
                                 </div>
                             </div>
+                            {
+                                Conference.map(conference=>(
                             <figure className="snip1527">
                                 <div className="image">
                                     <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/331810/pr-sample23.jpg"
                                          alt="pr-sample23"/>
                                 </div>
                                 <figcaption>
-                                    <div className="date"><span className="day">28</span><span className="month">Oct</span></div>
-                                    <h3>The World Ended Yesterday</h3>
+                                    <div className="date"><span className="day">{conference.Start_date.split("T")[0]}</span></div>
+                                    <h3>{conference.Title}
+                                    </h3>
                                     <p>
-
-                                        You know what we need, Hobbes? We need an attitude. Yeah, you can't be cool if you don't have
-                                        an attitude.
+                                        <strong> Description - {conference.Description}</strong>
+                                        <br/>
+                                        <strong> Organization - {conference.Organization}</strong>
+                                        <br/>
+                                        <strong> End Date - {conference.End_Date.split("T")[0]}</strong>
                                     </p>
                                 </figcaption>
                                 <a href="#"></a>
                             </figure>
-                            <figure className="snip1527">
-                                <div className="image">
-                                    <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/331810/pr-sample24.jpg" alt="pr-sample24"/>
+                                ))}
 
-                                </div>
-                                <figcaption>
-                                    <div className="date"><span className="day">17</span><span className="month">Nov</span></div>
-                                    <h3>An Abstract Post Heading</h3>
-                                    <p>
-
-                                        Sometimes the surest sign that intelligent life exists elsewhere in the universe is that none
-                                        of it has tried to contact us.
-                                    </p>
-                                </figcaption>
-                                <a href="#"></a>
-                            </figure>
-                            <figure className="snip1527">
-                                <div className="image">
-                                    <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/331810/pr-sample25.jpg"
-                                         alt="pr-sample25"/>
-                                </div>
-                                <figcaption>
-                                    <div className="date"><span className="day">01</span><span className="month">Dec</span></div>
-                                    <h3>Down with this sort of thing</h3>
-                                    <p>
-
-                                        I don't need to compromise my principles, because they don't have the slightest bearing on
-                                        what happens to me anyway.
-                                    </p>
-                                </figcaption>
-                                <a href="#"></a>
-                            </figure>
                         </div>
                     </section>
                     <section className="bg-light" id="portfolio">
                         <div className="container">
                             <div className="row">
                                 <div className="col-lg-12 text-center">
-                                    <h2 className="text-uppercase section-heading">Portfolio</h2>
+                                    <h2 className="text-uppercase section-heading">Keynote Speakers</h2>
                                     <h3 className="text-muted section-subheading">Lorem ipsum dolor sit amet
                                         consectetur.</h3>
                                 </div>
@@ -90,22 +104,23 @@ const Home = () => {
                             <section className="card-section-imagia">
                                 <div className="container">
                                     <div className="row">
+                                        {
+                                            Keynotes.map(keynotes=>(
                                         <div className="col-sm-6 col-md-4">
                                             <div className="card-container-imagia">
                                                 <div className="card-imagia">
                                                     <div className="front-imagia">
                                                         <div className="cover-imagia"><img alt=""
-                                                                                           src="https://unsplash.it/720/500?image=1067"/>
+                                                                                           src={keynotes.avatar}/>
                                                         </div>
                                                         <div className="user-imagia"><img className="img-circle" alt=""
-                                                                                          src="https://unsplash.it/120/120?image=64"/>
+                                                                                          src={keynotes.avatar}/>
                                                         </div>
                                                         <div className="content-imagia">
-                                                            <h3 className="name-imagia">John Doe</h3>
-                                                            <p className="subtitle-imagia">Subtitle </p>
-                                                            <p className="text-center"><em>Tantum autem cuique tribuendum, primum
-                                                                quantum ipse efficere possis, deinde etiam quantum ille quem diligas
-                                                                atque adiuves.</em></p>
+                                                            <h3 className="name-imagia">{keynotes.Name}</h3>
+                                                            <p className="subtitle-imagia">{keynotes.Designation} </p>
+                                                            <p className="text-center"><em>{keynotes.Description} </em></p>
+                                                            <p className="text-center"><em>{keynotes.Organization} </em></p>
                                                         </div>
                                                         <div className="footer-imagia"><span><i className="fa fa-plus"></i> More info</span>
                                                         </div>
@@ -131,86 +146,7 @@ const Home = () => {
                                                 </div>
                                             </div>
                                         </div>
-                                        <div className="col-sm-6 col-md-4">
-                                            <div className="card-container-imagia">
-                                                <div className="card-imagia">
-                                                    <div className="front-imagia">
-                                                        <div className="cover-imagia cover-gradient"></div>
-                                                        <div className="user-imagia"><img className="img-circle" alt=""
-                                                                                          src="https://unsplash.it/120/120?image=64"/>
-                                                        </div>
-                                                        <div className="content-imagia">
-                                                            <h3 className="name-imagia">John Doe</h3>
-                                                            <p className="subtitle-imagia">Subtitle </p>
-                                                            <p className="text-center"><em>Tantum autem cuique tribuendum, primum
-                                                                quantum ipse efficere possis, deinde etiam quantum ille quem diligas
-                                                                atque adiuves. </em></p>
-                                                        </div>
-                                                        <div className="footer-imagia"><span><i className="fa fa-plus"></i> More info</span>
-                                                        </div>
-                                                    </div>
-                                                    <div className="back-imagia">
-                                                        <div className="content-imagia content-back-imagia">
-                                                            <div>
-                                                                <h3 className="text-center">Lorem Ipsum</h3>
-                                                                <p className="text-center">Et hanc quidem praeter oppida multa duae
-                                                                    civitates exornant Seleucia opus Seleuci regis, et Claudiopolis
-                                                                    quam deduxit coloniam Claudius Caesar. Isaura enim antehac
-                                                                    nimium potens, olim subversa ut rebellatrix interneciva aegre
-                                                                    vestigia claritudinis pristinae monstrat admodum pauca. </p>
-                                                            </div>
-                                                        </div>
-                                                        <div className="footer-imagia">
-                                                            <div className="social-imagia text-center"><a href="#"><i
-                                                                className="fa fa-facebook"></i></a><a href="#"><i
-                                                                className="fa fa-linkedin"></i></a><a href="#"><i
-                                                                className="fa fa-twitter"></i></a></div>
-                                                        </div>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                        </div>
-                                        <div className="col-sm-6 col-md-4">
-                                            <div className="card-container-imagia">
-                                                <div className="card-imagia">
-                                                    <div className="front-imagia">
-                                                        <div className="cover-imagia"><img alt=""
-                                                                                           src="https://unsplash.it/720/500?image=1067"/>
-                                                        </div>
-                                                        <div className="user-imagia"><img className="img-circle" alt=""
-                                                                                          src="https://unsplash.it/120/120?image=64"/>
-                                                        </div>
-                                                        <div className="content-imagia">
-                                                            <h3 className="name-imagia">John Doe</h3>
-                                                            <p className="subtitle-imagia">Subtitle </p>
-                                                            <p className="text-center"><em>Tantum autem cuique tribuendum, primum
-                                                                quantum ipse efficere possis, deinde etiam quantum ille quem diligas
-                                                                atque adiuves. </em></p>
-                                                        </div>
-                                                        <div className="footer-imagia"><span><i className="fa fa-plus"></i> More info</span>
-                                                        </div>
-                                                    </div>
-                                                    <div className="back-imagia">
-                                                        <div className="content-imagia content-back-imagia">
-                                                            <div>
-                                                                <h3 className="text-center">Lorem Ipsum</h3>
-                                                                <p className="text-center">Et hanc quidem praeter oppida multa duae
-                                                                    civitates exornant Seleucia opus Seleuci regis, et Claudiopolis
-                                                                    quam deduxit coloniam Claudius Caesar. Isaura enim antehac
-                                                                    nimium potens, olim subversa ut rebellatrix interneciva aegre
-                                                                    vestigia claritudinis pristinae monstrat admodum pauca. </p>
-                                                            </div>
-                                                        </div>
-                                                        <div className="footer-imagia">
-                                                            <div className="social-imagia text-center"><a href="#"><i
-                                                                className="fa fa-facebook"></i></a><a href="#"><i
-                                                                className="fa fa-linkedin"></i></a><a href="#"><i
-                                                                className="fa fa-twitter"></i></a></div>
-                                                        </div>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                        </div>
+                                            ))}
                                     </div>
                                 </div>
                             </section>
